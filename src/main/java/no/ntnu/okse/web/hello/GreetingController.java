a2277e679843ad2e5afc49744036db78c8b6f1cd
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-02-25 12:09:07 +0100

    initated spring

---
+++ src/main/java/no/ntnu/okse/web/hello/GreetingController.java
@@ -0,0 +1,18 @@
+package no.ntnu.okse.web.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.String;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
