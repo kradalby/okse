8dca5b6958b5791d58d09f1b30b9dc02d363796b
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-03-18 13:59:00 +0100

    basic structure for add mapping

--- src/main/java/no/ntnu/okse/web/controller/ConfigController.java
+++ src/main/java/no/ntnu/okse/web/controller/ConfigController.java
@@ -1,5 +1,7 @@
 package no.ntnu.okse.web.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/config")
 public class ConfigController {
 
+    private static Logger log = Logger.getLogger(ConfigController.class.getName());
+
+
     @RequestMapping(method = RequestMethod.GET)
     public void config() {
         // should return configs
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="/mapping/add/{topic}/{newTopic}")
+    public void addMapping(@PathVariable String topic, @PathVariable String newTopic) {
+        log.info("Trying to add mapping between  " + topic + " --> " + newTopic);
     }
 
 }
