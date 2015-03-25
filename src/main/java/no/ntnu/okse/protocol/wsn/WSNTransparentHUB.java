3297f65de955733bc17bb7766c37b3a200794f9d
Trond Walleraunet <trond.walleraunet@gmail.com>
2015-03-25 13:45:59 +0100

    WSNTransparentHUB
    TransparentHUB

---
+++ src/main/java/no/ntnu/okse/protocol/wsn/WSNTransparentHUB.java
@@ -0,0 +1,48 @@
+package no.ntnu.okse.protocol.wsn;
+
+import org.ntnunotif.wsnu.base.internal.Hub;
+import org.ntnunotif.wsnu.base.internal.ServiceConnection;
+import org.ntnunotif.wsnu.base.util.InternalMessage;
+
+import java.io.OutputStream;
+import java.util.Collection;
+
+/**
+ * Created by Trond Walleraunet on 25.03.2015.
+ */
+public class WSNTransparentHUB implements Hub {
+    @Override
+    public InternalMessage acceptNetMessage(InternalMessage internalMessage, OutputStream outputStream) {
+        return null;
+    }
+
+    @Override
+    public InternalMessage acceptLocalMessage(InternalMessage internalMessage) {
+        return null;
+    }
+
+    @Override
+    public String getInetAdress() {
+        return null;
+    }
+
+    @Override
+    public void registerService(ServiceConnection serviceConnection) {
+
+    }
+
+    @Override
+    public void removeService(ServiceConnection serviceConnection) {
+
+    }
+
+    @Override
+    public boolean isServiceRegistered(ServiceConnection serviceConnection) {
+        return false;
+    }
+
+    @Override
+    public Collection<ServiceConnection> getServices() {
+        return null;
+    }
+}
