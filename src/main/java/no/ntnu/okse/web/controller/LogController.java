0c6e682f6b94172f77941c8651f7a6b0bd8e6007
Kristoffer Dalby <kradalby@kradalby.no>
2015-04-18 18:32:12 +0200

    reworked the controller to use streams and handle loglevels

--- src/main/java/no/ntnu/okse/web/controller/LogController.java
+++ src/main/java/no/ntnu/okse/web/controller/LogController.java
@@ -29,14 +29,18 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by kradalby on 17/04/15.
@@ -46,20 +50,59 @@ import java.util.List;
 @RequestMapping(value = "/api/log")
 public class LogController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ArrayList<Log> log() throws IOException {
-        ArrayList<Log> logs = new ArrayList<>();
-        File dir = new File("logs");
-        Collection<File> files = FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+    private static HashMap<String, ArrayList<String>> logLevels = new HashMap<String, ArrayList<String>>(){{
+        put("DEBUG", new ArrayList<String>(){{
+            add("DEBUG");
+            add("INFO");
+            add("WARN");
+        }});
+        put("INFO", new ArrayList<String>(){{
+            add("INFO");
+            add("WARN");
+        }});
+        put("WARN", new ArrayList<String>(){{
+            add("WARN");
+        }});
+    }};
 
-        for (File file : files) {
-            String name = file.getName();
-            List<String> lines = FileUtils.readLines(file);
-            Log log = new Log(name, lines);
-
-            logs.add(log);
+    public static boolean isWithinLogLevel(String logLevel, String line) {
+        for (String level : logLevels.get(logLevel)) {
+            if (line.contains(level)) return true;
         }
-        return logs;
+        return false;
+
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Log log(@RequestParam(value="logName", defaultValue="okse.log") String logName, @RequestParam(value="logLevel", defaultValue="DEBUG") String logLevel) {
+        //ArrayList<Log> logs = new ArrayList<>();
+        //File dir = new File("logs");
+        //Collection<File> files = FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+
+//        for (File file : files) {
+//            String name = file.getName();
+//            List<String> lines = FileUtils.readLines(file);
+//            Log log = new Log(name, lines);
+//
+//            logs.add(log);
+//        }
+
+        try {
+            Stream<String> l = Files.lines(Paths.get("logs/", logName));
+
+            List<String> lines = l.filter(x -> isWithinLogLevel(logLevel, x)).collect(Collectors.toList());
+            Collections.reverse(lines);
+            Log log = new Log(logName, lines);
+
+            return log;
+
+        } catch (IOException e) {
+            Log log = new Log("Does not exists", new ArrayList<String>() {{
+                add("The logfile you requested do not exist.");
+            }} );
+            return log;
+        }
     }
 
 }
