14161bbe36585feb0c404e0d51083eb025245350
mythern <myth@overflow.no>
2015-05-02 19:54:57 +0200

    Implement okse.properties configuration file support

--- src/main/java/no/ntnu/okse/core/Utilities.java
+++ src/main/java/no/ntnu/okse/core/Utilities.java
@@ -1,9 +1,16 @@
 package no.ntnu.okse.core;
 
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.TimeZone;
 
 /**
@@ -12,6 +19,8 @@ import java.util.TimeZone;
  * okse is licenced under the MIT licence.
  */
 public class Utilities {
+
+    public static Logger log = Logger.getLogger(Utilities.class.getName());
 
     /**
      * Returns a ISO 8601 HH:mm:ss.SSS formated string of a Duration object
@@ -24,4 +33,26 @@ public class Utilities {
         return format.format(new Date(millis - TimeZone.getDefault().getRawOffset()));
     }
 
+    /**
+     * Retrieve a set of properties from a .properties configuration file
+     * @param path The relative path to the configuration file (including filenam)
+     * @return A Properties object containing the data from the configuration file, null if errors during read.
+     */
+    public static Properties readConfigurationFromFile(String path) {
+        try {
+            File configFile = new File(path);
+            FileInputStream fis = new FileInputStream(configFile);
+            Properties properties = new Properties();
+            properties.load(fis);
+            fis.close();
+
+            return properties;
+        } catch (FileNotFoundException e) {
+            log.error("Configuration file did not exist: " + path);
+        } catch (IOException e) {
+            log.error("I/O error while attempting to read from configuration file: " + e.getMessage());
+        }
+
+        return null;
+    }
 }
