0332bd7e0339874d7dcd9e4e5766a5aefcd505b6
Freboto <toernvall@gmail.com>
2015-05-06 12:54:00 +0200

    AMPQ message subject is now set to, Message translated from OKSE, when it is sent from other protocol than AMQP

--- src/main/java/no/ntnu/okse/protocol/amqp/AMQPServer.java
+++ src/main/java/no/ntnu/okse/protocol/amqp/AMQPServer.java
@@ -231,7 +231,7 @@ public class AMQPServer extends BaseHandler {
         Section body = new AmqpValue(message.getMessage());
 
         msg.setAddress(AMQProtocolServer.getInstance().getHost() +"/" + message.getTopic());
-        msg.setSubject(message.getMessageID());
+        msg.setSubject("Message translated from OKSE");
         msg.setBody(body);
         return msg;
     }
