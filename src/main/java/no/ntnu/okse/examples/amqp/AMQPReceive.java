05ad2fd37f0d1490c4106a54db3ef197f5c6e4d5
Trond Walleraunet <trond.walleraunet@gmail.com>
2015-05-06 10:50:12 +0200

    fapfapfap amqpReceive eksempel kode

--- src/main/java/no/ntnu/okse/examples/amqp/AMQPReceive.java
+++ src/main/java/no/ntnu/okse/examples/amqp/AMQPReceive.java
@@ -71,7 +71,8 @@ public class AMQPReceive {
             }
         }
         if (addrs.size() == 0) {
-            addrs.add("amqp://m.fap.no:61050/bang/bang");
+            //addrs.add("amqp://m.fap.no:61050/bang/bang");
+            addrs.add("amqp://127.0.0.1:61050/bang/bang");
         }
     }
 
