d21cb78dce0985fd6fa6cb70e3736a4c16fc6177
Freboto <toernvall@gmail.com>
2015-05-07 16:37:39 +0200

    Changes SubscriptionHandlerTest.java to NgTest

--- src/test/java/no/ntnu/okse/protocol/amqp/SubscriptionHandlerTest.java
+++ src/test/java/no/ntnu/okse/protocol/amqp/SubscriptionHandlerTest.java
@@ -24,47 +24,60 @@
 
 package no.ntnu.okse.protocol.amqp;
 
-import junit.framework.TestCase;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class SubscriptionHandlerTest extends TestCase {
+import static org.testng.Assert.*;
 
+public class SubscriptionHandlerTest {
+
+    @BeforeMethod
     public void setUp() throws Exception {
-        super.setUp();
 
     }
 
+    @AfterMethod
     public void tearDown() throws Exception {
 
     }
 
+    @Test
     public void testGetAddress() throws Exception {
 
     }
 
+    @Test
     public void testGetAddress1() throws Exception {
 
     }
 
+    @Test
     public void testGetOutgoing() throws Exception {
 
     }
 
+    @Test
     public void testGetIncomming() throws Exception {
 
     }
 
+    @Test
     public void testOnLinkLocalOpen() throws Exception {
 
     }
 
+    @Test
     public void testOnConnectionUnbound() throws Exception {
 
     }
 
+    @Test
     public void testSubscriptionChanged() throws Exception {
 
     }
 
+    @Test
     public void testUnsubscribeAll() throws Exception {
 
     }
