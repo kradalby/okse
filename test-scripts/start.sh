435048955d587d8cbaceacc94fdf60a5cefd8009
Kristoffer Dalby <kradalby@kradalby.no>
2015-05-25 13:51:50 +0200

    latest start script

--- test-scripts/start.sh
+++ test-scripts/start.sh
@@ -4,9 +4,10 @@
 JVM_MEMORY="-Xms256m -Xmx512m"
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-JARFILE="$DIR/okse.jar"
+JARFILE="okse.jar"
 
 if which java >/dev/null; then
+    cd $DIR
     exec java -jar $JVM_MEMORY $JARFILE
     exit 0
 else
