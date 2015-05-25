5691fc3c7fa1c764a2480c771f967a50263691ab
Kristoffer Dalby <kradalby@kradalby.no>
2015-05-25 13:24:27 +0200

    cleanup

--- test-scripts/start.sh
+++ test-scripts/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Java VM memory settings. Minimum and maximum memory size.
-JVM_MEMORY="-Xms1024m -Xmx2048m"
+JVM_MEMORY="-Xms256m -Xmx512m"
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 JARFILE="$DIR/okse.jar"
 
-if test -n "$JAVA_HOME"; then
+if which java >/dev/null; then
     exec java -jar $JVM_MEMORY $JARFILE
-    exit 1
+    exit 0
 else
-    echo "JAVA_HOME is not set, exiting..."
-    exit 7
+    echo "Java is not found in your path, is it installed?"
+    exit 1
 fi
 
